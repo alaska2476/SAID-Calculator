6b1f7e96413fe3c7c1e592fd8e8d15491333f5b1
alaska2476 <anthonytoklo86@gmail.com>
2026-05-27 21:43:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -111,9 +111,7 @@ def build_table(prefix):
 
             # remove amount in this row
             col2.write("")
-
-            #  create 3 aligned rows
-            for j in range(3):
+            for j in range(10):
 
                 col1b, col2b = st.columns([1,1])
 
