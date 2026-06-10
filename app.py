acc9ccd2cf83e63eda44116da9aa94c05b5a9df8
alaska2476 <anthonytoklo86@gmail.com>
2026-06-10 09:48:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -288,9 +288,9 @@ if len(st.session_state.history) > 0:
  #  SHOW TOTAL ON SCREEN
     if total > 0:
     total_label = "TOTAL OVERPAYMENT"
-elif total < 0:
+    elif total < 0:
     total_label = "TOTAL UNDERPAYMENT"
-else:
+    else:
     total_label = "TOTAL NO CHANGE"
 
 st.subheader(total_label)
