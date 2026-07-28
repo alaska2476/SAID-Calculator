670e1d0d8643a173a3b4b34e93e8a00b0adb01dd
alaska2476 <anthonytoklo86@gmail.com>
2026-07-28 13:16:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,21 @@ def get_amounts(comm, group, year, month, adults, children):
         ((df["Children"].isna()) | (df["Children"] >= children))
     ]
 
-    return sorted(df["Amount"].dropna().unique())
+   st.dataframe(
+    df[
+        [
+            "Benefit",
+            "Tier",
+            "Adults",
+            "Children",
+            "Start_Date",
+            "End_Date",
+            "Amount"
+        ]
+    ]
+)
+
+return sorted(df["Amount"].dropna().unique())
 
 # =========================
 # HEADER
