8bc22bc4a0b669c989efb2a0817bd34a058bc7e9
alaska2476 <anthonytoklo86@gmail.com>
2026-08-07 11:59:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -306,7 +306,7 @@ with col_a:
         st.markdown("### Benefit Issued:")
 with col_b:
     st.markdown(
-        "<div style='padding-top:6px;'></div>",
+        "<div style='padding-top:2px;'></div>",
         unsafe_allow_html=True
     )
 
