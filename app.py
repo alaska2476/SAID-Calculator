37d7368c98881870c0017ea6509aaaafccc6a879
alaska2476 <anthonytoklo86@gmail.com>
2026-08-07 09:41:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -292,7 +292,7 @@ declared_benefit = declared_total - declared_net_total
 
 budget_deficit = max(actual_total - total_income_considered, 0)
 
-c1, c2, spacer, c3 = st.columns([3, 4, 2, 2])
+c1, c2, spacer, c3 = st.columns([3, 4, 1, 2])
 
 with c1:
     st.markdown(
